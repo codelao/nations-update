b91ddee6ff4fb1bcd5a4ff084bdcdcc525164ebe
codelao <sethlaolu@gmail.com>
2018-09-08 13:05:25 +0100

    make it better

--- app.js
+++ app.js
@@ -11,16 +11,14 @@ const port = process.env.PORT || 3001
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use('/api', apiRouter)
-
-
+app.use(express.static(path.join(__dirname, 'build')));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-app.get('*', (req, res)=> {
-  app.use(express.static(path.join(__dirname, 'build')));
+app.use('/api', apiRouter)
+app.get('/', (req, res)=> {
   res.sendFile(path.join(__dirname+'build/index.html'))
 })
 
